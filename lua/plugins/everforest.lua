2e12ad9b485d9f9b752f42cd88cb3e09e8929f59
Michael Janssen <github@michaeljanssen.nl>
2025-07-24 14:59:30 +0200

    wip

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,12 @@
+return {
+  { "neanias/everforest-nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
+
